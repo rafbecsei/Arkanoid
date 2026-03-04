3292e9136785888aa59cf08e59b6b05c2538ba9e
EricSongWatanabe <ericsongwatanabe1@gmail.com>
2026-03-04 16:05:25 -0300

    Criação da fase 2 e implementação do display de vidas.

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -1,32 +1,71 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BlockSpawner : MonoBehaviour
 {
-    public GameObject[] blockPrefabs;
-
-    public int linhas = 6;
-    public int colunas = 10;
-    public float espacamento = 0.03f;
+    public GameObject[] blockPrefabs; // 0: R (roxo), 1: V (verde), 2: A (azul), 3: Vm (vermelho), 4: Am (amarelo), 5: C (cinza)
 
     public Vector2 StartPosition = new Vector2(-3.72f, 3f);
+    public float espacamento = 0.03f;
+    public float colunaWidth = 0.8f;
+    public float linhaHeight = 0.35f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for(int i = 0; i < linhas; i++)
+        int fase = SceneManager.GetActiveScene().buildIndex;
+
+        if(fase == 1)
         {
-            for(int j = 0; j < colunas; j++)
+            // Fase1: layout padrão 6x10
+            int linhas = 6;
+            int colunas = 10;
+
+            for(int i = 0; i < linhas; i++)
             {
-                Vector2 posicao = new Vector2(StartPosition.x + j * (0.8f + espacamento), 
-                                            StartPosition.y - i * (0.35f + espacamento)); 
-                GameObject bloco = Instantiate(blockPrefabs[i], posicao, Quaternion.identity);
+                for(int j = 0; j < colunas; j++)
+                {
+                    Vector2 posicao = new Vector2(
+                        StartPosition.x + j * (colunaWidth + espacamento),
+                        StartPosition.y - i * (linhaHeight + espacamento)
+                    );
+
+                    GameObject prefab = blockPrefabs[i % (blockPrefabs.Length - 1)]; // exceto cinza
+                    Instantiate(prefab, posicao, Quaternion.identity);
+                }
             }
         }
-    }
+        else if(fase == 2)
+        {
+            // Fase2: escada da direita para a esquerda
+            int colunasEscada = 10; // número de colunas
+            int[] degrausPorColuna = {1,2,3,4,5,6,7,8,9,10}; // número de blocos coloridos por coluna
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            for(int c = 0; c < colunasEscada; c++)
+            {
+                int degraus = degrausPorColuna[c];
+                for(int l = 0; l <= degraus; l++)
+                {
+                    Vector2 posicao = new Vector2(
+                        StartPosition.x + (colunasEscada - 1 - c) * (colunaWidth + espacamento), // da direita para a esquerda
+                        StartPosition.y - l * (linhaHeight + espacamento)
+                    );
+
+                    GameObject prefab;
+
+                    if(l == 0)
+                    {
+                        // base cinza
+                        prefab = blockPrefabs[5]; // C
+                    }
+                    else
+                    {
+                        // cores na sequência: R, V, A, Vm, Am
+                        prefab = blockPrefabs[(l - 1) % 5]; // 0-4
+                    }
+
+                    Instantiate(prefab, posicao, Quaternion.identity);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,9 +12,16 @@ public class GameManager : MonoBehaviour
 
     private GameObject currentBall;
 
+    public GameObject paddleRedPrefab;
+    public Vector2 vidasStartPos = new Vector2(-3.5f, -4.5f);
+    public float espacamentoEntreVidas = 1f;
+    private List<GameObject> vidasVisuais;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SpawnVidasVisuais();
+
         SpawnBall();
 
         blocos = GameObject.FindGameObjectsWithTag("Block").Length;
@@ -30,6 +38,14 @@ public class GameManager : MonoBehaviour
     public void PerdeVida()
     {
         vidas--;
+
+        // Remove visual da vida perdida
+        if (vidasVisuais.Count > 0)
+        {
+            GameObject ultimaVida = vidasVisuais[vidasVisuais.Count - 1];
+            vidasVisuais.RemoveAt(vidasVisuais.Count - 1);
+            Destroy(ultimaVida);
+        }
 
         if(vidas > 0)
         {
@@ -56,6 +72,19 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
+
+    void SpawnVidasVisuais()
+    {
+        vidasVisuais = new List<GameObject>();
+
+        for (int i = 0; i < vidas; i++)
+        {
+            Vector3 pos = new Vector3(vidasStartPos.x + i * espacamentoEntreVidas, vidasStartPos.y, 0);
+            GameObject vida = Instantiate(paddleRedPrefab, pos, Quaternion.identity);
+            vidasVisuais.Add(vida);
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
